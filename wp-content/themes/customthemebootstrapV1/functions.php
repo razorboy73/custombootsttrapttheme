6eea3b5c32432d93297d1e355b522e7c8501931d
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-08 13:21:55 -0500

    added bootstrap js in footer

--- wp-content/themes/customthemebootstrapV1/functions.php
+++ wp-content/themes/customthemebootstrapV1/functions.php
@@ -5,9 +5,9 @@
 function theme_files(){
     
     wp_enqueue_style( 'main-style', get_template_directory_uri().'/style.css');
-
-
     wp_enqueue_style( 'bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+
+    wp_enqueue_script( 'bootstrap-js',"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array("jquery"), true, true );
 
 }
 
--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -17,7 +17,7 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" >
+    <? php wp_footer()?>
 
     
   </body>
