bb57f30d69674be1e433a00bce47693a2638ecee
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-08 11:06:36 -0500

    replacing head and enqueing style sheets

--- wp-content/themes/customthemebootstrapV1/functions.php
+++ wp-content/themes/customthemebootstrapV1/functions.php
@@ -4,6 +4,8 @@
 
 function theme_files(){
     
+    wp_enqueue_style( 'main-style', get_template_directory_uri().'/style.css');
+
 
     wp_enqueue_style( 'bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
 
--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -5,10 +5,11 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
-
-    <title>Hello, world!</title>
+    <title><?php wp_title(); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
   </head>
   <body>
     <h1>Hello, world!</h1>
