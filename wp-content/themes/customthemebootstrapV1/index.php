bde40a36b03a5a941eeed71adb77ded53acdf93f
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-08 10:40:44 -0500

    meta charset added

--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
