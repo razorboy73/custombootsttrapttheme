fced2800472e68a5b372c1bfb67a5de8ed065684
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-08 22:09:08 -0500

    js not in footer

--- wp-content/themes/customthemebootstrapV1/functions.php
+++ wp-content/themes/customthemebootstrapV1/functions.php
@@ -7,7 +7,7 @@ function theme_files(){
     wp_enqueue_style( 'main-style', get_template_directory_uri().'/style.css');
     wp_enqueue_style( 'bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
 
-    wp_enqueue_script( 'bootstrap-js',"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array("jquery"), true, true );
+    wp_enqueue_script( 'bootstrap-js',"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array("jquery"), true, false );
 
 }
 
