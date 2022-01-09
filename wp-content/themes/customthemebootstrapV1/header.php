14a54829272802a21cb173a90e34c53e0780a7b8
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-09 12:25:29 -0500

    added border bottom to nav menu

--- wp-content/themes/customthemebootstrapV1/header.php
+++ wp-content/themes/customthemebootstrapV1/header.php
@@ -14,7 +14,7 @@
   <body <?php body_class(); ?>>
 
 <!--Navbar-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light small custom-nav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light small custom-nav border-bottom">
   <div class="container">
     <a class="navbar-brand" href="<?php echo bloginfo('url')?>">
       <img src="<?php echo get_template_directory_uri();?>/img/frshminds-logo-186-40-logo.png" alt="">
