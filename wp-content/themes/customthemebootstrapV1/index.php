af04223a50a30e6131b12d6c96edfece4c3ceca7
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-09 11:11:27 -0500

    Broke out footer and header

--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?>
-  </head>
-  <body>
+<?php get_header(); ?>
     <h1>Hello, world!</h1>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <?php wp_footer()?>
-
-    
-  </body>
-</html>
+  <?php get_footer(); ?>
