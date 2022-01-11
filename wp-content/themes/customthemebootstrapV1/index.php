1c723bfd3a030fa8e77aa9ee5669b88e8f5dbc27
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-11 08:15:30 -0500

    initial index layout

--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
-    <h1>Hello, world!</h1>
+<section class="default-holder">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-9 col-md-9">
+        <div class="list-group">
+          <a href="#" class="list-group-item">
+            <div class="row">
+              <div class="col-lg-4 col-md-4">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="<?php echo get_the_title();?>" >
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-3"></div>
+    </div>
+  </div>
+</section>
 
   <?php get_footer(); ?>
