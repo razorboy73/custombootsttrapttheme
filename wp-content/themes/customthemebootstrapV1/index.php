31a910e6e903f160d9ff7e4ff14255648f5aaf55
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-14 07:27:52 -0500

    items centered

--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -5,7 +5,7 @@
       <div class="col-lg-9 col-md-9">
         <div class="list-group">
           <a href="#" class="list-group-item list-group-item-action small">
-            <div class="row">
+            <div class="row align-items-center">
               <div class="col-lg-4 col-md-4">
                 <img class="img-fluid rounded" src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="<?php echo get_the_title();?>" >
               </div>
