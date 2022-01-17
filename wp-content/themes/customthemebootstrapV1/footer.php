bcd0b27be7d0b5044dfc2b26fa31b747fe073ff5
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-16 20:27:46 -0500

    footer

--- wp-content/themes/customthemebootstrapV1/footer.php
+++ wp-content/themes/customthemebootstrapV1/footer.php
@@ -1,3 +1,17 @@
+<footer class="main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3">
+                <div class="footer_widget">
+                    <h4 class="fs-6">Footer Widget Title</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
 <?php wp_footer()?>
 
     
