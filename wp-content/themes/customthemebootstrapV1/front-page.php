0061db673a9156f23bc41d6d4925496ca9792a46
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-18 20:59:31 -0500

    front page

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
 Hello World From Front Page
-
+Hello World From Front Page
+Hello World From Front Page
+Hello World From Front Page
+Hello World From Front Page
+Hello World From Front Page
 <?php get_footer(); ?>
