49236bebc3e9306f2bf6a2da98c11bd36196fea1
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-20 10:15:43 -0500

    fonts

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -14,7 +14,7 @@ get_header(); ?>
      <div class="row">
          <div class="col-lg-6 col-md-6">
              <div class="banner-text">
-                 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                 <h1 class="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolor optio quae maxime tenetur, totam et dolore. Autem maiores dolore consequuntur sed cumque, provident hic aspernatur reiciendis maxime cum possimus.</p>
              </div>
          </div>
