9b3fb80e30e8b10236248f88600f79a8ba3afac9
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-20 10:25:47 -0500

    aligned items

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <header class="default-holder main-header pt-3 pb-3">
  <div class="container">
-     <div class="row">
+     <div class="row align-item-center">
          <div class="col-lg-6 col-md-6">
              <div class="banner-text">
                  <h1 class="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
