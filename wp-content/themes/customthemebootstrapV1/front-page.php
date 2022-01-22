dd9b27e283977b9c40fcf16450e915ec217dc71a
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-22 10:36:57 -0500

    modal

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -16,12 +16,30 @@ get_header(); ?>
          <?php if ( is_active_sidebar( 'front_page_banner_text' ) ) : ?>
 			      <?php dynamic_sidebar( 'front_page_banner_text' ); ?>
             <?php endif; ?>
-            <a href="#" class="btn btn-sm btn-success">
+            <a href="#" class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-</svg>
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </svg>
                 Sign Up To Get Started</a>
          </div>
+         <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
          <div class="col-lg-6 col-md-6">
          <?php if ( is_active_sidebar( 'front_page_banner_image' ) ) : ?>
 			      <?php dynamic_sidebar( 'front_page_banner_image' ); ?>
