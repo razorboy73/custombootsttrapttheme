904c6b2cb0793146a5834f32af660aab962ee974
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-22 09:48:06 -0500

    front page work

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -16,10 +16,6 @@ get_header(); ?>
          <?php if ( is_active_sidebar( 'front_page_banner_text' ) ) : ?>
 			      <?php dynamic_sidebar( 'front_page_banner_text' ); ?>
             <?php endif; ?>
-             <div class="banner-text">
-                 <h1 class="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolor optio quae maxime tenetur, totam et dolore. Autem maiores dolore consequuntur sed cumque, provident hic aspernatur reiciendis maxime cum possimus.</p>
-             </div> 
          </div>
          <div class="col-lg-6 col-md-6">
              <img class="img-fluid rounded" src="<?php echo get_template_directory_uri();?>/img/placeholder.jpg" alt="" srcset="">
