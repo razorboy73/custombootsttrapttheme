cb5e16389f5f5de8b0560b93809e101a4dfbd450
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-22 10:23:00 -0500

    buttton

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -16,7 +16,11 @@ get_header(); ?>
          <?php if ( is_active_sidebar( 'front_page_banner_text' ) ) : ?>
 			      <?php dynamic_sidebar( 'front_page_banner_text' ); ?>
             <?php endif; ?>
-            <a href="#" class="btn btn-sm btn-success"></a>
+            <a href="#" class="btn btn-sm btn-success">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg>
+                Sign Up To Get Started</a>
          </div>
          <div class="col-lg-6 col-md-6">
          <?php if ( is_active_sidebar( 'front_page_banner_image' ) ) : ?>
