6e71f05f5a63c8d88d0dc99b7206023b7f4a4478
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-23 07:58:31 -0500

    form

--- wp-content/themes/customthemebootstrapV1/footer.php
+++ wp-content/themes/customthemebootstrapV1/footer.php
@@ -32,11 +32,15 @@
                     </div>
                     <div class="modal-body">
                         <form>
-                            <input type="text" class="form-control form-control-sm" placeholder="Your Name" required>
-                            <input type="email" class="form-control form-control-sm" placeholder="Your email" required>
+                            <input type="text" class="form-control form-control-sm mb-3" placeholder="Your Name" required>
+                            <input type="email" class="form-control form-control-sm mb-3" placeholder="Your email" required>
                             <div class="d-grid gap-2">
                                 
-                                <button class="btn btn-primary" type="submit">Sign Up to Get Started</button>
+                                <button class="btn btn-primary" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                    </svg>
+                                    Sign Up to Get Started</button>
                             </div>
                         </form>
                     </div>
