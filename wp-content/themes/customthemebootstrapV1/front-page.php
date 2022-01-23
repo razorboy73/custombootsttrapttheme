ad3ccfbaca500386beb2a24a5fd915c21ff72727
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-23 12:10:15 -0500

    fixed content

--- wp-content/themes/customthemebootstrapV1/front-page.php
+++ wp-content/themes/customthemebootstrapV1/front-page.php
@@ -81,7 +81,7 @@ get_header(); ?>
 
 <section class="course_cat">
     <div class="container">
-        <div class="row">
+        <div class="row d-flex justify-content-center">
             <div class="col-lg-6 col-md-6">
                 <div class="course_cat_text">
                     <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit</h2>
