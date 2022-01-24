4577f120f5e0eca7f23f8eb1c426652a8b85a20b
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-24 07:25:43 -0500

    page

--- wp-content/themes/customthemebootstrapV1/page.php
+++ wp-content/themes/customthemebootstrapV1/page.php
@@ -8,7 +8,18 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             <div class="col-lg-6 col-md-6">
                 <div class="page-title-heading">
                     <h1><?php echo get_the_title() ?></h1>
+                    <p><?php echo get_the_excerpt() ?></p>
                 </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                    <?php
+                    if ( has_post_thumbnail() ) { ?>
+                    <img class="img-fluid rounded" src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php echo get_the_title();?>" >
+                    
+                <?php }else{ ?>
+                    <img class="img-fluid rounded" src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="<?php echo get_the_title();?>" >
+                    
+                    <?php } ?>
             </div>
         </div>
     </div>
