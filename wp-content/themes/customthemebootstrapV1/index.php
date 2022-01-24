fa65e63e3cb924b9bd16516bba42436bc6c9ccdf
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-24 08:20:20 -0500

    sidebar

--- wp-content/themes/customthemebootstrapV1/index.php
+++ wp-content/themes/customthemebootstrapV1/index.php
@@ -34,10 +34,8 @@
         </div>
       </div>
       <div class="col-lg-3 col-md-3">
-        <?php if ( is_active_sidebar( 'default_sidebar' ) ) : ?>
-			      <?php dynamic_sidebar( 'default_sidebar' ); ?>
-            <?php endif; ?>
-	      
+      
+	      <?php get_sidebar(); ?>
 
       </div>
     </div>
