bc38ab60089b8b667187e492275f430ebbacf69c
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-25 13:33:31 -0500

    mb

--- wp-content/themes/customthemebootstrapV1/single.php
+++ wp-content/themes/customthemebootstrapV1/single.php
@@ -32,7 +32,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             <div class="col-lg-9 col-md-9">
                 <article class="main-content small">
                     <?php the_content();?>
-                    <div class="alert alert-secondary mb-0" role="alert">
+                    <div class="alert alert-secondary mb-2" role="alert">
                     <p class="postmetadata"><?php _e("Posted in "); ?><?php the_category(', ') ?></p>
                     </div>
                     <?php comments_template(); ?>
