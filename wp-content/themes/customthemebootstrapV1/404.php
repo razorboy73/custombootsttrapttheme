480ca61b182d216db9e14241823521f9383c9673
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-26 17:12:38 -0500

    404

---
+++ wp-content/themes/customthemebootstrapV1/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<section class="404-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <h1>404 - Page Not Found</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
