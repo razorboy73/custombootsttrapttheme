699a3bfb9fe6c6c4e27cbe08510ca4c003dcb412
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-26 17:26:09 -0500

    404

--- wp-content/themes/customthemebootstrapV1/404.php
+++ wp-content/themes/customthemebootstrapV1/404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-<section class="404-page default-holder pt-3 pb-3">
+<section class="404-page default-holder pt-5 pb-5">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-lg-6 col-md-6">
-                <h1>404 - Page Not Found</h1>
+                <h1 class="text-center">404 - Page Not Found</h1>
             </div>
         </div>
     </div>
