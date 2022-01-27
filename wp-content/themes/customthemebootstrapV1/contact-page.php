e4f8037bcdee0e05b7dff8af1d876cfc690c7a23
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-26 23:14:02 -0500

    sd

--- wp-content/themes/customthemebootstrapV1/contact-page.php
+++ wp-content/themes/customthemebootstrapV1/contact-page.php
@@ -10,7 +10,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.947985749117!2d-79.41610034858455!3d43.6700515790182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34901f06ef99%3A0x420e8b8abb8549f2!2s5%20Olive%20Ave%2C%20Toronto%2C%20ON%20M6G%201T7!5e0!3m2!1sen!2sca!4v1643256691404!5m2!1sen!2sca" width="100%" height=auto style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.947985749117!2d-79.41610034858455!3d43.6700515790182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34901f06ef99%3A0x420e8b8abb8549f2!2s5%20Olive%20Ave%2C%20Toronto%2C%20ON%20M6G%201T7!5e0!3m2!1sen!2sca!4v1643256691404!5m2!1sen!2sca" width="100%" height="450px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
     </div>
