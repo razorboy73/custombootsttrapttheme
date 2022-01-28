cc0dd63accbf79a3f795b29c065050167394a9b0
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-27 22:18:39 -0500

    dta

--- wp-content/themes/customthemebootstrapV1/contact-page.php
+++ wp-content/themes/customthemebootstrapV1/contact-page.php
@@ -25,7 +25,55 @@
                     <?php endif; ?>
             </div>
             <div class="col-lg-8 col-md-8">
-                        <?php the_field("hours");?>
+            $table = get_field( 'hours' );
+
+                if ( ! empty ( $table ) ) {
+
+                    echo '<table border="0">';
+
+                        if ( ! empty( $table['caption'] ) ) {
+
+                            echo '<caption>' . $table['caption'] . '</caption>';
+                        }
+
+                        if ( ! empty( $table['header'] ) ) {
+
+                            echo '<thead>';
+
+                                echo '<tr>';
+
+                                    foreach ( $table['header'] as $th ) {
+
+                                        echo '<th>';
+                                            echo $th['c'];
+                                        echo '</th>';
+                                    }
+
+                                echo '</tr>';
+
+                            echo '</thead>';
+                        }
+
+                        echo '<tbody>';
+
+                            foreach ( $table['body'] as $tr ) {
+
+                                echo '<tr>';
+
+                                    foreach ( $tr as $td ) {
+
+                                        echo '<td>';
+                                            echo $td['c'];
+                                        echo '</td>';
+                                    }
+
+                                echo '</tr>';
+                            }
+
+                        echo '</tbody>';
+
+                    echo '</table>';
+                }
             </div>
         </div>
     </div>
