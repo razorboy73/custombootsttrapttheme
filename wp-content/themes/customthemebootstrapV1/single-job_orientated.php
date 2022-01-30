c883b3aaec47c0c0f33d28a576228dfee69ff5f1
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-30 15:43:54 -0500

    d

--- wp-content/themes/customthemebootstrapV1/single-job_orientated.php
+++ wp-content/themes/customthemebootstrapV1/single-job_orientated.php
@@ -43,7 +43,7 @@
                 <?php
                     $value = get_field("course_description_image");
                     if ($value){ ?>
-                       <img src ="<?php echo $value; ?>" alt=""> 
+                       <img src ="<?php echo $value; ?>" alt="<?php get_the_title(); ?>"> 
                   <?php  } else {
                         echo "empty";
                     };
