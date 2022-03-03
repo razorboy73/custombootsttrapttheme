df78fca3646cd527b5c8e6ec7979e7af03ccd679
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-03-03 11:25:40 -0500

    working on it

--- wp-content/themes/mycustomthemeV1/footer.php
+++ wp-content/themes/mycustomthemeV1/footer.php
@@ -9,20 +9,6 @@
            wp_nav_menu(array(
                "theme_location" => "footer-nav",
                 'menu'            => '',
-                'container'       => 'div',
-                'container_class' => '',
-                'container_id'    => '',
-                'menu_class'      => 'menu',
-                'menu_id'         => '',
-                'echo'            => true,
-                'fallback_cb'     => 'wp_page_menu',
-                'before'          => '',
-                'after'           => '',
-                'link_before'     => '',
-                'link_after'      => '',
-                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>', 
-                'depth' => 0, 
-                'walker' => ''
            ));
 
            ?>
